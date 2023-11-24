d471fa154bb2a9ffb0453be9935c2cde446b5d3f
Christian Manzaraz <magnusmanz@gmail.com>
2023-11-24 19:17:38 -0300

    Initial Commit

---
+++ newsApiCall/newsApiCallApp.swift
@@ -0,0 +1,17 @@
+//
+//  newsApiCallApp.swift
+//  newsApiCall
+//
+//  Created by Christian Manzaraz on 24/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct newsApiCallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
