7c336556388acab420639e479daf59313e4007b8
Christian Manzaraz <magnusmanz@gmail.com>
2023-11-24 21:43:23 -0300

    Everything is working on

---
+++ newsApiCall/Model/DataModel.swift
@@ -0,0 +1,8 @@
+//
+//  DataModel.swift
+//  newsApiCall
+//
+//  Created by Christian Manzaraz on 24/11/2023.
+//
+
+import Foundation
---
+++ newsApiCall/ViewModel/ApiCall.swift
@@ -0,0 +1,8 @@
+//
+//  ApiCall.swift
+//  newsApiCall
+//
+//  Created by Christian Manzaraz on 24/11/2023.
+//
+
+import Foundation
